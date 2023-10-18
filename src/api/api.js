92928d3568486b90fa8c11f4f587ba7b4e391341
francorinque <francoagustinrinquecorreo@gmail.com>
2023-10-18 17:16:55 -0300

    change baseUrl deploy

--- src/api/api.js
+++ src/api/api.js
@@ -1,14 +1,15 @@
-import axios from 'axios'
+import axios from "axios"
 
 export const instance = axios.create({
-	baseURL: 'http://localhost:3001',
+  // baseURL: 'http://localhost:3001',
+  baseURL: "https://rgames-backend-production.up.railway.app",
 })
 
 export const searchGame = async ({ name }) => {
-	try {
-		const { data } = await instance.get(`?name=${name}`)
-		return data
-	} catch (error) {
-		console.log(error)
-	}
+  try {
+    const { data } = await instance.get(`?name=${name}`)
+    return data
+  } catch (error) {
+    console.log(error)
+  }
 }
